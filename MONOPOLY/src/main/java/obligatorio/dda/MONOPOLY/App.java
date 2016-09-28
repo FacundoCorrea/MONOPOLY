d6928a2add4f685ac421e0bc9e8aa3f4c043a4f8
FacundoCorrea <correa.faco@gmail.com>
2016-09-28 19:53:36 -0300

    JAVA

---
+++ MONOPOLY/src/main/java/obligatorio/dda/MONOPOLY/App.java
@@ -0,0 +1,13 @@
+package obligatorio.dda.MONOPOLY;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
