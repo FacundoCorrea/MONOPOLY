caa350b282b0b4231d2f9b84dcc6d7fa876f3743
emersonprior <emersonprior@hotmail.com>
2016-11-15 21:15:44 -0300

    correccion jugador

--- MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/carta.java
+++ MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/carta.java
@@ -1,27 +1,16 @@
 package net.franciscovillegas.cei.obligatorio.server.entities;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
-import javax.persistence.Entity;
-import javax.persistence.Table;
 
-@Entity
-@Table(name = "cartas")
 public class carta {
-private String Tipo;
-private String Descripcion;
-private int Monto;
-private int Estado;
-public carta(String tipo, String descripcion, int monto, int estado) {
-	super();
-	Tipo = tipo;
-	Descripcion = descripcion;
-	Monto = monto;
-	Estado = estado;
-}
+	private String Tipo;
+	private String Descripcion;
+	private int Monto;
+	private int Estado;
+		public carta(String tipo, String descripcion, int monto, int estado) {
+			super();
+			Tipo = tipo;
+			Descripcion = descripcion;
+			Monto = monto;
+			Estado = estado;
+		}
 
 }
