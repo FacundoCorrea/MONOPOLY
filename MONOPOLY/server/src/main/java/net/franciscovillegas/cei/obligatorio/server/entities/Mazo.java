a34be482dca1ac16d5e01ad67de3f85c25cb1fa8
emersonprior <emersonprior@hotmail.com>
2016-11-15 22:03:21 -0300

    mazo
    serqwetqretgqertqt

--- MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/Mazo.java
+++ MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/Mazo.java
@@ -1,5 +1,21 @@
 package net.franciscovillegas.cei.obligatorio.server.entities;
 
+import java.util.List;
+
 public class Mazo {
-private List<carta>cartas
+private List<Carta> cartas;
+
+public List<Carta> getCartas() {
+	return cartas;
+}
+
+public void setCartas(List<Carta> cartas) {
+	this.cartas = cartas;
+}
+
+public Mazo(List<Carta> cartas) {
+
+	this.cartas = cartas;
+}
+
 }
