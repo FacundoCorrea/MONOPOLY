f952a7e047dd1ab3a8cfb727edbf38cfae6b8c99
emersonprior <emersonprior@hotmail.com>
2016-11-15 21:36:06 -0300

    Carta
    Cartita viste

--- MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/carta.java
+++ MONOPOLY/server/src/main/java/net/franciscovillegas/cei/obligatorio/server/entities/carta.java
@@ -1,16 +1,41 @@
 package net.franciscovillegas.cei.obligatorio.server.entities;
 
-public class carta {
+public class Carta {
 	private String Tipo;
 	private String Descripcion;
 	private int Monto;
 	private int Estado;
-		public carta(String tipo, String descripcion, int monto, int estado) {
-			super();
-			Tipo = tipo;
-			Descripcion = descripcion;
-			Monto = monto;
-			Estado = estado;
-		}
+	public String getTipo() {
+		return Tipo;
+	}
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+	public String getDescripcion() {
+		return Descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		Descripcion = descripcion;
+	}
+	public int getMonto() {
+		return Monto;
+	}
+	public void setMonto(int monto) {
+		Monto = monto;
+	}
+	public int getEstado() {
+		return Estado;
+	}
+	public void setEstado(int estado) {
+		Estado = estado;
+	}
+	public Carta(String pTipo, String pDescripcion, int pMonto, int pEstado) {
+
+		this.Tipo = pTipo;
+		this.Descripcion = pDescripcion;
+		this.Monto = pMonto;
+		this.Estado = pEstado;
+	}
+
 
 }
